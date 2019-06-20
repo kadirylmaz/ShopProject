ed4d33340e0eb44542ca41360d6e63955d6f8ef5
Kadir Yılmaz <kadirylmaz02@gmail.com>
2019-06-20 15:45:54 +0300

    Core Katmanında geliştirmeler yapıldı.

---
+++ ShopProject/ShopProject.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShopProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopProject.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T: class, IEntity, new()
+    {
+        T Get(Expression<Func<T, bool>> filter = null);
+
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+
+        T Add(T entity);
+
+        T Update(T entity);
+
+        T Delete(T entity);
+    }
+}
