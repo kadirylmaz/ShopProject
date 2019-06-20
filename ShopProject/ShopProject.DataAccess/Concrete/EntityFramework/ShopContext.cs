cdf9e3659b37fbb8855b8b8e4e4eaeb916ecb40f
Kadir Yılmaz <kadirylmaz02@gmail.com>
2019-06-20 22:30:49 +0300

    ShopDbContext oluşturuldu.(DataAccess Katmanı)

---
+++ ShopProject/ShopProject.DataAccess/Concrete/EntityFramework/ShopContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopProject.Entities.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopProject.DataAccess.Concrete.EntityFramework
+{
+    public class ShopContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-4IMGOMO\KADIR; Database=ShopProjectDb; Integrated security=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(x => new { x.OrderID, x.ProductID });
+        }
+        public DbSet<MenuItem> MenuItems  { get; set; }
+
+        public DbSet<SubMenu> SubMenus { get; set; }
+
+        public DbSet<SubSubMenu> SubSubMenus { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Shipper> Shippers { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+       
+    }
+}
