5e43f321b830314942bb92fc188f7486558d695e
Kadir Yılmaz <kadirylmaz02@gmail.com>
2019-06-21 17:22:51 +0300

    Businness katmanında MenuItem Service yazıldı.

---
+++ ShopProject/ShopProject.Bussiness/Abstract/IMenuItemService.cs
@@ -0,0 +1,21 @@
+﻿using ShopProject.Entities.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopProject.Bussiness.Abstract
+{
+    public interface IMenuItemService
+    {
+        List<MenuItem> GetList();
+
+        MenuItem Get(int id);
+
+        void Add(MenuItem menuItem);
+
+        void Update(MenuItem menuItem);
+
+        void Delete(MenuItem menuItem);
+
+    }
+}
---
+++ ShopProject/ShopProject.Bussiness/Concrete/MenuItemManager.cs
@@ -0,0 +1,44 @@
+﻿
+using ShopProject.Bussiness.Abstract;
+using ShopProject.DataAccess.Abstract;
+using ShopProject.Entities.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopProject.Bussiness.Concrete
+{
+    public class MenuItemManager : IMenuItemService
+    {
+        private IMenuItemDAL _menuItemDAL;
+        public MenuItemManager(IMenuItemDAL menuItemDAL)
+        {
+            _menuItemDAL = menuItemDAL;
+        }
+
+        public void Add(MenuItem menuItem)
+        {
+            _menuItemDAL.Add(menuItem);
+        }
+
+        public void Delete(MenuItem menuItem)
+        {
+            _menuItemDAL.Delete(menuItem);
+        }
+
+        public MenuItem Get(int id)
+        {
+            return _menuItemDAL.Get(x => x.ID == id);
+        }
+
+        public List<MenuItem> GetList()
+        {
+            return _menuItemDAL.GetList();
+        }
+
+        public void Update(MenuItem menuItem)
+        {
+            _menuItemDAL.Update(menuItem);
+        }
+    }
+}
